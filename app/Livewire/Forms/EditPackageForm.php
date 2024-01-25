55cf6adf7bd4a3489b8d0c7eb285c9b6c19bab06
ezb_ryankenidy <kenidyryan@gmail.com>
2024-01-25 10:58:56 +0700

    edit package

--- app/Livewire/Forms/EditPackageForm.php
+++ app/Livewire/Forms/EditPackageForm.php
@@ -7,23 +7,28 @@ use Livewire\Attributes\Validate;
 use Livewire\Form;
 use Livewire\WithFileUploads;
 use App\Models\Package;
+use Illuminate\Support\Facades\Storage;
 
 class EditPackageForm extends Form
 {
     use WithFileUploads;
     public ?Package $package;
 
-
     public $id;
 
+    public $code = '';
     public $name = '';
+    public $valid_end = '';
+    public $status = '';
     public $image;
 
     public function setPackage(Package $package)
     {
         $this->id = $package->id;
+        $this->code = $package->code;
         $this->name = $package->name;
-        $this->image = $package->image;
+        $this->valid_end = $package->valid_end;
+        $this->status = $package->status;
     }
 
     public function rules()
@@ -32,5 +37,28 @@ class EditPackageForm extends Form
             'name' => ['required', 'min:3', Rule::unique('packages')->ignore($this->id)],
             'image' =>  ''
         ];
+    }
+
+    public function update($package)
+    {
+        if ($this->image) {
+            $trimmedImagePath = str_replace('storage/', '', $package->image);
+            Storage::disk('public')->delete($trimmedImagePath);
+
+            $fileName = $this->image->getClientOriginalName();
+
+            $imageName = now()->timestamp . '_' . $fileName;
+            $imagePath = $this->image->storeAs('img', $imageName, 'public');
+
+            $this->image = 'storage/' . $imagePath;
+        } else {
+            $this->image = $package->image;
+        }
+
+        $package->update(
+            $this->all()
+        );
+
+        $this->reset('image');
     }
 }
--- resources/views/livewire/pages/package/detail.blade.php
+++ resources/views/livewire/pages/package/detail.blade.php
@@ -56,6 +56,16 @@ new class extends Component {
         ];
     }
 
+    public function editPackage() {
+        $this->editPackageForm->validate();
+
+        $this->editPackageForm->update($this->package);
+
+        $this->dispatch('close-modal', 'edit-package');
+        $this->dispatch('open-alert', name: 'edit-package-alert', type: 'Success', message: 'Package Updated Successfully !');
+
+    }
+
     public function searchPackageDetails() {
         $this->resetPage();
     }
@@ -94,11 +104,35 @@ new class extends Component {
 
 <div class="">
     <x-alert name="add-detail-alert"></x-alert>   
-    <x-alert name="alert"></x-alert>   
+    <x-alert name="edit-package-alert"></x-alert>
 
     <h1 class="mb-4 font-bold text-2xl">View Packages</h1>
 
     <x-tabs :tabs="['Main', 'Detail']">
+        <x-slot name="dropdown">
+            <x-dropdown class="ml-auto" >
+                <x-slot name="trigger">
+                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md bg-gray-800 text-gray-300 hover:text-gray-100 focus:outline-none transition ease-in-out duration-150 " >
+                        <div>Actions</div>
+
+                        <div class="ms-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <x-dropdown-button
+                    x-on:click.prevent="$dispatch('open-modal', 'edit-package')"
+                    >
+                        {{ __('Edit') }}
+                    </x-dropdown-button>
+                </x-slot>
+            </x-dropdown>
+        </x-slot>
+
         <x-slot name="content">
             <div class="bg-gray-800 text-white p-8 rounded-md">
                 <div  x-show="activeTab === 0" class="rounded-r-md grid sm:grid-cols-2 gap-4">
@@ -205,4 +239,89 @@ new class extends Component {
         </x-slot>
     </x-tabs>
 
+    <x-modal name="edit-package" :show="false">
+        <form wire:submit.prevent="editPackage" class="p-6 text-gray-800" enctype="multipart/form-data">
+
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Edit Package') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600">
+                {{ __('') }}
+            </p>
+
+            <div class="mt-6">
+                <x-input-label for="code" value="{{ __('Start Time') }}"  />
+
+                <x-text-input
+                    model="editPackageForm.code"
+                    id="code"
+                    name="code"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Code') }}"
+                />
+
+                <x-input-error :messages="$errors->get('editPackageForm.code')" class="mt-2" />
+            </div>
+
+            <div class="mt-6">
+                <x-input-label for="name" value="{{ __('Name') }}"  />
+
+                <x-text-input
+                    model="editPackageForm.name"
+                    id="name"
+                    name="name"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Name') }}"
+                />
+
+                <x-input-error :messages="$errors->get('editPackageForm.name')" class="mt-2" />
+            </div>
+
+            <div class="mt-6">
+                <x-input-label for="valid_end" value="{{ __('Valid End') }}"  />
+
+                <x-text-input
+                    model="editPackageForm.valid_end"
+                    id="valid_end"
+                    type="date"
+                    name="valid_end"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Valid End') }}"
+                />
+
+                <x-input-error :messages="$errors->get('editPackageForm.valid_end')" class="mt-2" />
+            </div>
+
+            <div class="mt-6">
+                <x-input-label for="image" value="{{ __('Image') }}"  />
+
+                <x-text-input
+                    model="editPackageForm.image"
+                    id="image"
+                    type="file"
+                    name="image"
+                    accept="image/*"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Image') }}"
+                />
+
+                <x-input-error :messages="$errors->get('editPackageForm.image')" class="mt-2" />
+            </div>
+
+            <div>
+                <x-input-error :messages="$errors->get('editPackageForm.id')" class="mt-2" />
+            </div>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close');">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-primary-button class="ms-3">
+                    {{ __('Save') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </x-modal>
 </div>
